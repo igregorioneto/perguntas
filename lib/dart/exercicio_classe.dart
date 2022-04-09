44eaa1affebce3404bdcc4ac7b1e1774be58d96c
Gregório <igregoriodoneto@outlook.com>
2022-04-09 19:16:49 -0300

    Exercícios de classes

--- lib/dart/exercicio_classe.dart
+++ lib/dart/exercicio_classe.dart
@@ -1,3 +1,31 @@
+imprimirProduto(int qtd, {String nome = '', double preco = 0}) {
+  for (var i = 0; i < qtd; i++) {
+    print('Produto é $nome e seu valor é R\$$preco !!!');
+  }
+}
+
+class Produto {
+  String nome;
+  double preco;
+
+  // Produto(String nome, double preco) {
+  //   this.nome = nome;
+  //   this.preco = preco;
+  // }
+
+  // Parâmetros posicionais
+  // Produto(this.nome, this.preco);
+  // Parâmetros nomeados
+  // Produto({this.nome, this.preco});
+
+  Produto(this.nome, this.preco);
+}
+
 main() {
   print('Trabalhando com classes!');
+  // Criando um objeto
+  // var p1 = Produto(nome: 'Caneta', preco: 4.99);
+  var p1 = Produto('Caneta', 4.99);
+  print('Produto é ${p1.nome} e seu valor é R\$${p1.preco}');
+  imprimirProduto(20, nome: p1.nome, preco: p1.preco);
 }
