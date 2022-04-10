31938a6c9e8de8d50b142b729991f4f334aa144b
Gregório <igregoriodoneto@outlook.com>
2022-04-10 16:37:10 -0300

    Construindo tela de perguntas no estilo do flutter 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(ParguntaApp());
-}
+main() => runApp(ParguntaApp());
 
 class ParguntaApp extends StatelessWidget {
   // const ParguntaApp({Key? key}) : super(key: key);
 
+  final List<String> perguntas = [
+    'Qual sua cor favorita?',
+    'Qual seu animal de estimação favorito?'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Hello World!!!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            // Esta forma é a anterior ao flutter 2
+            // RaisedButton(
+            //   child: Text('Resposta 1'),
+            //   onPressed: null,
+            // ),
+            const ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            const ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            const ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
