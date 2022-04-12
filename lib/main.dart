430728b1bd097dccdd483e7b6b0bdb8f1baa7d3f
Gregório <igregoriodoneto@outlook.com>
2022-04-12 20:48:30 -0300

    Passando valores por parametro para o componente Resposta

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:perguntas/questao.dart';
+import 'package:perguntas/resposta.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -9,7 +10,6 @@ class _PerguntaAppState extends State<PerguntaApp> {
   void _responder() {
     setState(() => _perguntaSelecionada);
     _perguntaSelecionada++;
-    print(_perguntaSelecionada);
   }
 
   @override
@@ -27,23 +27,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(_perguntas[_perguntaSelecionada]),
-            // Esta forma é a anterior ao flutter 2
-            // RaisedButton(
-            //   child: Text('Resposta 1'),
-            //   onPressed: null,
-            // ),
-            ElevatedButton(
-              child: const Text('Resposta 1'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: const Text('Resposta 2'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: const Text('Resposta 3'),
-              onPressed: _responder,
-            )
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),
       ),
@@ -52,8 +38,6 @@ class _PerguntaAppState extends State<PerguntaApp> {
 }
 
 class PerguntaApp extends StatefulWidget {
-  // const ParguntaApp({Key? key}) : super(key: key);
-
   @override
   _PerguntaAppState createState() {
     return _PerguntaAppState();
---
+++ lib/resposta.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() quandoSelecionado;
+
+  const Resposta(this.texto, this.quandoSelecionado);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text(texto),
+        onPressed: quandoSelecionado,
+      ),
+    );
+  }
+}
