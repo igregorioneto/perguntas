1c15c5790fbb3371ddfb2c6ffd9b5bc782cb352d
Gregório <igregoriodoneto@outlook.com>
2022-04-15 13:49:59 -0300

    Finalizando projeto perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
     }
   }
 
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
+  }
+
   bool get temPerguntaSelecionada {
     return _perguntaSelecionada < _perguntas.length;
   }
@@ -68,7 +75,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder,
               )
-            : Resultado(),
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
